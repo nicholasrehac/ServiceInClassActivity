572789e09b55c2d25b5f86e53bd6792aa7516bf5
Nicholas Rehac <tup88289@temple.edu>
2024-04-16 12:00:44 -0400

    action menu controls timer service

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -8,6 +8,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
+import android.util.Log
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -27,6 +33,27 @@ class MainActivity : AppCompatActivity() {
 
     var timerHandler: Handler? = null
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.stop_menu_button -> {
+                timerBinder?.stop()
+                true
+            }
+            R.id.play_menu_button -> {
+                timerBinder?.start(10)
+                true
+            }
+            R.id.pause_menu_button -> {
+                timerBinder?.pause()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
